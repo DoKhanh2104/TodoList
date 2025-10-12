9bb82cf91664850cf8a513488fdfd63e6b0d08a5
dohungquockhanh <winwin020104@gmail.com>
2025-10-12 18:23:58 +0700

    prepare to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,20 +3,33 @@ import taskRoute from './routes/tasksRouters.js'
 import { connectDB } from './config/db.js'
 import dotenv from 'dotenv'
 import cors from 'cors'
-
-
-const app = express()
-//middlewares
-app.use(express.json())
-app.use(cors({origin: "http://localhost:5173"}))
+import path from 'path'
 
 dotenv.config()
 
+const app = express()
+
 
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve( )
+
+//middlewares
+app.use(express.json())
+
+if(process.env.NODE_ENV !== 'production'){
+    app.use(cors({origin: "http://localhost:5173"}))
+}
 
 
 app.use("/api/tasks", taskRoute)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*" , (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    })
+}
+
 
 
 //kết nối rồi mới chạy server
